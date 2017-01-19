778ff68f5ed62347569e4c0f7ff2072809f8b628
yakir <yakir@yakir-PC.Home>
2017-01-19 22:51:52 +0200

    Started Working on CLI

--- src/main/java/com/hit/view/CLI.java
+++ src/main/java/com/hit/view/CLI.java
@@ -2,11 +2,20 @@ package com.hit.view;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class CLI 
 {
 	private InputStream in;
 	private OutputStream out;
+	private List<String> syntax = new ArrayList<>(Arrays.asList(
+			"start",
+			"stop",
+			"LRU",
+			"MFU",
+			"Second Chance"));
 	
 	public CLI(InputStream in, OutputStream out)
 	{
