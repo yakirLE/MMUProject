2ff96e4d37c9605590751fe026b3a18eccd56f0d
yakir <yakir@yakir-PC.Home>
2017-01-19 20:48:55 +0200

    Declerations of third part are done

---
+++ src/main/java/com/hit/driver/MMUDriver.java
@@ -0,0 +1,11 @@
+package com.hit.driver;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class MMUDriver 
+{
+	public static void main(String[] args) throws InterruptedException, InvocationTargetException 
+	{
+		
+	}
+}
---
+++ src/main/java/com/hit/view/CLI.java
@@ -0,0 +1,26 @@
+package com.hit.view;
+
+import java.io.InputStream;
+import java.io.OutputStream;
+
+public class CLI 
+{
+	private InputStream in;
+	private OutputStream out;
+	
+	public CLI(InputStream in, OutputStream out)
+	{
+		this.in = in;
+		this.out = out;
+	}
+	
+	public String[] getConfiguration()
+	{
+		
+	}
+	
+	public void write(String string)
+	{
+		
+	}
+}
